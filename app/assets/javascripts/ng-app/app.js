71d816edbe05ff0013302b8646010584798ccab5
Alejandro Sobko <asobko@devartis.com>
2016-08-09 21:04:05 -0300

    Project skeleton added

---
+++ app/assets/javascripts/ng-app/app.js
@@ -0,0 +1,32 @@
+angular
+    .module('myApp', ['ngAnimate', 'ui.router', 'templates'])
+
+    .config(function ($stateProvider, $urlRouterProvider, $locationProvider) {
+        $stateProvider
+            .state('home', {
+                url: '/',
+                templateUrl: '/assets/home.html',
+                controller: 'HomeCtrl'
+            })
+            .state('dashboard', {
+                abstract: true,
+                url: '/dashboard',
+                templateUrl: 'dashboard/layout.html'
+            })
+            .state('dashboard.one', {
+                url: '',
+                templateUrl: 'dashboard/one.html'
+            })
+            .state('dashboard.two', {
+                url: '/two',
+                templateUrl: 'dashboard/two.html'
+            })
+            .state('dashboard.three', {
+                url: '/three',
+                templateUrl: 'dashboard/three.html'
+            });
+
+
+        $urlRouterProvider.otherwise('/');
+
+    });
