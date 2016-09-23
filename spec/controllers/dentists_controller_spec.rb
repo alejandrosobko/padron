ab2b616d2f16a2deab10938a9b58327dd1fea6d5
Alejandro Sobko <asobko@devartis.com>
2016-09-22 23:15:48 -0300

    Add empty tests

--- spec/controllers/dentists_controller_spec.rb
+++ spec/controllers/dentists_controller_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-RSpec.describe VisitsController, type: :controller do
+RSpec.describe DentistsController, type: :controller do
 
   it 'to be implemented' do
     assert true
--- spec/controllers/visitors_controller_spec.rb
+++ spec/controllers/visitors_controller_spec.rb
@@ -2,4 +2,8 @@ require 'rails_helper'
 
 RSpec.describe VisitorsController, type: :controller do
 
+  it 'to be implemented' do
+    assert true
+  end
+
 end
