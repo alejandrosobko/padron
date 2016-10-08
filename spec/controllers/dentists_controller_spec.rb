a9ca9ea8eea215599e1b13f0b11a7a5966d36ba9
Alejandro Sobko <asobko@devartis.com>
2016-10-08 17:42:33 -0300

    Remove unnecessary code

--- spec/controllers/dentists_controller_spec.rb
+++ spec/controllers/dentists_controller_spec.rb
@@ -3,12 +3,6 @@ require 'rails_helper'
 RSpec.describe DentistsController, type: :controller do
 
   describe 'GET index' do
-    let(:dentist) {build(:dentist, :complete)}
-
-    before(:each) do
-      allow(Dentist).to receive(:create).and_return dentist
-    end
-
     it 'returns an empty array' do
       get :index
       expect(response.status).to eq 200
