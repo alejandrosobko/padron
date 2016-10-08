35d76c690e591c3cfe4117a9fa07ebf2e7f346ca
Alejandro Sobko <asobko@devartis.com>
2016-10-08 14:58:55 -0300

    Add model test to visit

--- spec/models/visit_spec.rb
+++ spec/models/visit_spec.rb
@@ -5,7 +5,16 @@ RSpec.describe Visit, :type => :model do
   describe 'empty visit' do
     it 'should not can save an empty visit' do
       visit = Visit.new
-      expect(visit.save).to eq(false)
+      expect(visit.save).to be false
+    end
+
+    it 'should can save a new visit with dentist, visit and visit date' do
+      dentist = Dentist.new
+      visitor = Visitor.new
+      visit = Visit.new({dentist: dentist, visitor: visitor, visit_date: DateTime.new})
+
+      expect(visit.observations).to be nil
+      expect(visit.save).to be true
     end
   end
 
