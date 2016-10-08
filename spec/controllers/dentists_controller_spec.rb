03f7dec1d70baf4b6a540862f334570f855793d2
Alejandro Sobko <asobko@devartis.com>
2016-10-08 17:03:21 -0300

    Add test in dentist controller test

--- spec/controllers/dentists_controller_spec.rb
+++ spec/controllers/dentists_controller_spec.rb
@@ -2,8 +2,18 @@ require 'rails_helper'
 
 RSpec.describe DentistsController, type: :controller do
 
-  it 'to be implemented' do
-    assert true
+  describe 'GET index' do
+    let(:dentist) {build(:dentist, :complete)}
+
+    before(:each) do
+      allow(Dentist).to receive(:create).and_return dentist
+    end
+
+    it 'returns an empty array' do
+      get :index
+      expect(response.status).to eq 200
+      expect(response.body).to eq "#{[]}"
+    end
   end
 
 end
