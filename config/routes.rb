4181634068df1e61541bacff1b1c6f59f4c211a6
Alejandro Sobko <asobko@devartis.com>
2016-10-08 22:41:47 -0300

    update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -2,12 +2,7 @@ Rails.application.routes.draw do
 
   root 'application#index'
 
-  get 'dentists/:id' => 'dentists#show'
-  get 'dentists' => 'dentists#index'
-  put 'dentists/:id' => 'dentists#update'
-  delete 'dentists/:id' => 'dentists#destroy'
-  get 'visits' => 'visits#index'
-  post 'visits' => 'visits#create'
+  resources :dentists
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
