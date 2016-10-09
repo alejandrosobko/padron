de6273abc8f42549698ed4c168ad5ab77f78ca99
Alejandro Sobko <asobko@devartis.com>
2016-10-08 22:40:28 -0300

    Remove visit controller and visitor controller

--- spec/controllers/visitors_controller_spec.rb
+++
@@ -1,9 +0,0 @@
-require 'rails_helper'
-
-RSpec.describe VisitorsController, type: :controller do
-
-  it 'to be implemented' do
-    assert true
-  end
-
-end
--- spec/controllers/visits_controller_spec.rb
+++
@@ -1,9 +0,0 @@
-require 'rails_helper'
-
-RSpec.describe VisitsController, type: :controller do
-
-  it 'to be implemented' do
-    assert true
-  end
-
-end
