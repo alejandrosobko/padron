18c28e4a2a359eb9263ed04e738f3d0d4d6cd01a
Alejandro Sobko <asobko@devartis.com>
2016-10-15 04:13:24 -0300

    Save dentist with multiple days

--- spec/models/dentist_spec.rb
+++ spec/models/dentist_spec.rb
@@ -18,4 +18,19 @@ RSpec.describe Dentist, :type => :model do
     end
   end
 
+  describe 'with work calendar' do
+    let(:visit) { build(:visit, visitor: build(:visitor)) }
+    let(:dentist) { build(:dentist, visits: [visit], work_calendar: WorkCalendar.new) }
+
+    it 'should can save a dentist with a lot of workable days' do
+      hours = [build(:workable_hour_morning), build(:workable_hour_afternoon)]
+      days = [build(:workable_day_monday, workable_hours: hours), build(:workable_day_tuesday, workable_hours: hours)]
+      dentist.work_calendar.workable_days = days
+
+      expect(dentist.save).to be true
+      expect(dentist.work_calendar.workable_days.first.day).to eq 'Monday'
+      expect(dentist.work_calendar.workable_days[1].day).to eq 'Tuesday'
+    end
+  end
+
 end
