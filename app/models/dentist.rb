2d48316d66d44e396e9f9d0c7b6873cff63d470a
Alejandro Sobko <asobko@devartis.com>
2016-10-28 00:07:32 -0300

    Allow nil enrollments

--- app/models/dentist.rb
+++ app/models/dentist.rb
@@ -2,7 +2,7 @@ class Dentist < ActiveRecord::Base
   has_many :visits, dependent: :destroy
   has_one :work_calendar, dependent: :destroy, autosave: true
   validates_presence_of :visits, :work_calendar
-  validates :enrollment, uniqueness: {message: 'Ya existe un odontólogo con esa matrícula'}
+  validates :enrollment, uniqueness: {message: 'Ya existe un odontólogo con esa matrícula'}, allow_nil: true
   accepts_nested_attributes_for :visits, :work_calendar
 
   def as_json(options = {})
