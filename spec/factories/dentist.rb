4c9c166ca68a0eaa556cb9494632b55a62790e48
Alejandro Sobko <asobko@devartis.com>
2016-10-29 05:08:43 -0300

    Fix Dentist Factory

--- spec/factories/dentist.rb
+++ spec/factories/dentist.rb
@@ -4,13 +4,13 @@ FactoryGirl.define do
     name 'Alejandro'
     surname 'Sobko'
     enrollment 1234
-    location 'Quilmes'
-    institution 'TIP'
-    street 'Siempreviva'
-    number 123
-    telephone 42789393
-    cellphone 1512233445
-    email 'ale@sobko.com'
+    locations ['Quilmes']
+    institutions ['TIP']
+    streets ['Siempreviva']
+    numbers [123]
+    telephones []
+    cellphones [1512233445, 15933827466]
+    emails ['ale@sobko.com']
     specialty 'Rails development'
   end
 
