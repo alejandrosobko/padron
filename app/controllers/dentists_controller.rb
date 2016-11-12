83d05be23c0b71e6878629e00f2a845bd23ef306
Alejandro Sobko <asobko@devartis.com>
2016-11-12 19:39:39 -0300

    Remove log when update a dentist

--- app/controllers/dentists_controller.rb
+++ app/controllers/dentists_controller.rb
@@ -25,7 +25,6 @@ class DentistsController < ApplicationController
       destroy_hours(params[:dentist][:hours_to_remove])
       destroy_days(params[:dentist][:days_to_remove])
       dentist.update!(dentist_params)
-      log(dentist)
       render json: dentist
     rescue => e
       render json: dentist.errors.messages, status: :unprocessable_entity
@@ -55,11 +54,6 @@ class DentistsController < ApplicationController
         work_calendar_attributes: [:dentist_id, :id,
                                    workable_days_attributes: [:work_calendar_id, :id, :day,
                                                               workable_hours_attributes: [:id, :from, :to, :workable_day_id]]])
-  end
-
-  def log(dentist)
-    @logger ||= Logger.new("#{Rails.root}/log/dentist_update.log")
-    @logger.info("\n[+] Dentist with id = #{dentist.id} was updated: updated_at: #{dentist.updated_at}, visited by: #{dentist.visits.last.visitor.name} (visitor_id: #{dentist.visits.last.visitor.id})")
   end
 
 end
