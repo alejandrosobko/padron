25ef87e45c9d1dc763ec3da27e25d309a30892e4
Alejandro Sobko <asobko@devartis.com>
2016-11-12 01:50:25 -0300

    Logging when update a dentist

--- app/controllers/dentists_controller.rb
+++ app/controllers/dentists_controller.rb
@@ -25,6 +25,7 @@ class DentistsController < ApplicationController
       destroy_hours(params[:dentist][:hours_to_remove])
       destroy_days(params[:dentist][:days_to_remove])
       dentist.update!(dentist_params)
+      log(dentist)
       render json: dentist
     rescue => e
       render json: dentist.errors.messages, status: :unprocessable_entity
@@ -50,11 +51,15 @@ class DentistsController < ApplicationController
   def dentist_params
     params.require(:dentist).permit(
         :name, :surname, :enrollment, :specialty, :attention_datetime, locations: [], institutions: [], streets: [], numbers: [],
-        telephones: [], cellphones: [], emails:[], visits_attributes: [:id, :visit_date, :observations, visitor_attributes: [:id, :name]],
+        telephones: [], cellphones: [], emails: [], visits_attributes: [:id, :visit_date, :observations, visitor_attributes: [:id, :name]],
         work_calendar_attributes: [:dentist_id, :id,
                                    workable_days_attributes: [:work_calendar_id, :id, :day,
                                                               workable_hours_attributes: [:id, :from, :to, :workable_day_id]]])
   end
 
+  def log(dentist)
+    @logger ||= Logger.new("#{Rails.root}/log/dentist_update.log")
+    @logger.info("\n[+] Dentist with id = #{dentist.id} was updated: updated_at: #{dentist.updated_at}, visited by: #{dentist.visits.last.visitor.name} (visitor_id: #{dentist.visits.last.visitor.id})")
+  end
 
 end
